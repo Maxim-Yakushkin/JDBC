f448bf47425964172ada092d35a6f46295440787
Maxim <max-yakushkin1@yandex.ru>
2021-03-09 21:47:28 +0300

    Подключение к БД

---
+++ src/main/java/lesson_03_Connection_To_DB/JdbcRunner.java
@@ -0,0 +1,14 @@
+package lesson_03_Connection_To_DB;
+
+import util.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JdbcRunner {
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
+    }
+}
---
+++ src/main/java/util/ConnectionManager.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private static final String URL = "jdbc:postgresql://localhost:5432/flight_repository";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "pass";
+
+    public ConnectionManager() {
+    }
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
